a749aee79db8f5550e5e6c3972422a0c4427c560
Daniel <dalejodc@gmail.com>
2018-09-14 13:27:43 -0600

    Validations

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -13,18 +13,49 @@ export class TemplateComponent implements OnInit {
 
 	constructor() { }
 
+	user: Object={
+		name: null,
+		username: null,
+		email:null
+	}
+
 	ngOnInit() {
 		$('.ui.checkbox').checkbox();
+
 	}
 
-	save(tryForm:NgForm){
+	save(tryForm?:NgForm){
+		$('#qqform form')
+		.form({
+			on: 'blur',
+			inline: false,
+			fields: {
+				email: {
+					identifier  : 'email',
+					rules: [
+					{
+						type   : 'empty',
+						prompt : 'Please enter a value'
+					}
+					]
+				}
+			}
+		});
+
 		console.log("Post");
 		console.log('Complete form:',tryForm);
-		console.log('Value',tryForm.value);
+		console.log(this.user);
 	}
 
-	showModal(): void{
-		$('.ui.modal').modal('show');
+	close(){
+		$('.message .close')
+		.on('click', function() {
+			$(this)
+			.closest('.message')
+			.transition('fade')
+			;
+		})
+		;
 	}
 
 }
