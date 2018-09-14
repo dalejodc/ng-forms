0e1e89fb073fe0b13552d236d9aff9a3a5b2bcd9
Daniel <dalejodc@gmail.com>
2018-09-13 23:53:05 -0600

    Adding Semantic-ui via npm

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+// import * as $ from 'jquery';
+declare var $:any;
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+	selector: 'app-template',
+	templateUrl: './template.component.html',
+	styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  	
-  }
+	ngOnInit() {
 
-  save(){
-  	console.log("Post");
-  }
+	}
+
+	save(tryForm:NgForm){
+		console.log("Post");
+		console.log('Complete form:',tryForm);
+		console.log('Value',tryForm.value);
+	}
+
+	showModal(): void{
+		$('.ui.modal').modal('show');
+	}
 
 }
